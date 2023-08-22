4da90db7e504051c9f661b8eee1f1414fb95faef
Federico Peirano <fedepr2345@gmail.com>
2023-08-22 13:36:46 -0300

    Email Funcionando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+const btnBuy = document.getElementById('btn_buy');
 let loginForm = document.querySelector('.login-form');
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
 
 document.querySelector('#login-btn').onclick = () =>{
    loginForm.classList.add('active');
@@ -7,9 +10,6 @@ document.querySelector('#login-btn').onclick = () =>{
 document.querySelector('#close-login-form').onclick = () =>{
    loginForm.classList.remove('active');
 }
-
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .nav');
 
 menu.onclick = () =>{
    menu.classList.toggle('fa-times');
@@ -27,3 +27,29 @@ window.onscroll = () =>{
       document.querySelector('.header').classList.remove('active');
    }
 }
+
+btnBuy.addEventListener('click', function (event) {
+   console.log("Evento de clic agregado");
+   event.preventDefault();
+
+   btnBuy.innerHTML = 'Enviando...';
+
+   const serviceID = 'service_jg0hz5r';
+   const templateID = 'template_yqk5m8x';
+
+   const templateParams = {
+      to_email: 'fedepr2345@gmail.com',
+      subject: 'Gracias por tu compra',
+      message: '¡Gracias por tu compra en Premiur Air!'
+   };
+
+   emailjs.send(serviceID, templateID, templateParams)
+   .then(() => {
+      btnBuy.innerHTML = 'Buy'; 
+      alert('Correo de agradecimiento enviado con éxito.');
+   })
+   .catch((err) => {
+      btnBuy.innerHTML = 'Buy'; 
+      alert('Error al enviar el correo de agradecimiento: ' + JSON.stringify(err));
+   });
+});
