82f95afc23f822fe37022abb44637f0cba6176ea
Federico Peirano <fedepr2345@gmail.com>
2023-08-23 15:56:44 -0300

    Comiteo lo que falta

---
+++ Views/Layouts/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Premium Air</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- bootstrap cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../Premium-Air/Util/css/style.css">
+   <link rel="stylesheet" href="../Premium-Air/Util/css/style.scss">
+
+   <link rel="icon" type="image/x-icon" href="../Premium-Air/Util/images/favicon.ico">
+
+   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7789689269935618"
+     crossorigin="anonymous"></script>
+
+</head>
+<body>
+   
+<!-- header section starts     -->
+
+<header class="header fixed-top">
+
+   <div class="container">
+
+      <div class="row align-items-center">
+
+         <a href="#" class="logo mr-auto"> <i class="fas fa-cloud"></i> Premium Air  </a>
+
+         <nav class="nav">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#menu">Menu</a>
+            <a href="#blogs">Membership</a>
+            <a href="#contact">Contact</a>
+         </nav>
+
+         <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <div id="login-btn" class="fas fa-user"></div>
+         </div>
+
+      </div>
+
+   </div>
+
+</header>
+
+<!-- login form starts -->
+<div class="login-form">
+
+   <form action="">
+      <div id="close-login-form" class="fas fa-times"></div>
+      <a href="#" class="logo mr-auto"> <i class="fas fa-cloud"></i> Premium Air </a>
+      <h3>let's start a new great day</h3>
+      <input type="email" name="" placeholder="enter your email" id="" class="box">
+      <input type="password" name="" placeholder="enter your password" id="" class="box">
+      <div class="flex">
+         <input type="checkbox" name="" id="remember-me">
+         <label for="remember-me">remember me</label>
+         <a href="#">forgot password?</a>
+      </div>
+      <input type="submit" value="login now" class="link-btn">
+      <p class="account">don't have an account? <a href="#">create one!</a></p>
+   </form>
+
+</div>
